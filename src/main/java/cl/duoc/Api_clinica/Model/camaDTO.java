4973e3ef23765fcf2d78f61252218dd953821527
Sigrid <si.monsalve@duocuc.cl>
2026-06-01 01:12:59 -0400

    Creación DTO en clinica model

---
+++ src/main/java/cl/duoc/Api_clinica/Model/camaDTO.java
@@ -0,0 +1,15 @@
+package cl.duoc.Api_clinica.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class camaDTO {
+        private String id;
+        private int prioridad;
+}
