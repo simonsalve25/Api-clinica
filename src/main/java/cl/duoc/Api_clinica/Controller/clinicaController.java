7803bc54fa7686c3a6671e1b6f14937c6366e1f0
Sigrid <si.monsalve@duocuc.cl>
2026-06-03 00:56:11 -0400

    actualización controller clinica

--- src/main/java/cl/duoc/Api_clinica/Controller/clinicaController.java
+++ src/main/java/cl/duoc/Api_clinica/Controller/clinicaController.java
@@ -1,5 +1,6 @@
 package cl.duoc.Api_clinica.Controller;
 
+import cl.duoc.Api_clinica.Service.clinicaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +13,39 @@ public class clinicaController {
     private clinicaService service;
 
     @PostMapping
-    public ResponseEntity<clinica>
-    registrar(@Valid @RequestBody clinica clinica){
+    public ResponseEntity<Paciente>
+    registrar(@Valid @RequestBody Paciente paciente){
 
         return ResponseEntity.ok(
-                service.registrar(clinica));
+                service.registrar(paciente));
     }
 
     @GetMapping
-    public List<clinica> listar(){
+    public List<Paciente> listar(){
         return service.obtenerTodos();
     }
 
     @GetMapping("/criticos")
-    public List<clinica> criticos(){
+    public List<Paciente> criticos(){
         return service.obtenerCriticos();
+    }
+
+    @PutMapping("/{id}/evolucion")
+    public Paciente actualizar(
+            @PathVariable Long id,
+            @RequestBody String evolucion){
+
+        return service.actualizarEvolucion(
+                id,
+                evolucion);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(
+            @PathVariable Long id){
+
+        service.eliminar(id);
+
+        return ResponseEntity.ok().build();
     }
 }
